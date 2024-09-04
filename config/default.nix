8c17859409bad1c84b4886353e177a917b85ec83
Patrick <fwy996602672@gmail.com>
2024-09-04 18:13:52 +0800

    add one dark

--- config/default.nix
+++ config/default.nix
@@ -66,8 +66,9 @@
   ];
   options = {
     theme = lib.mkOption {
-      default = lib.mkDefault "paradise";
+      default = lib.mkDefault "onedark";
       type = lib.types.enum [
+        "onedark"
         "paradise"
         "decay"
         "edge-dark"
@@ -90,7 +91,7 @@
   };
   config = {
     # The base16 theme to use, if you want to use another theme, change it in colorscheme.nix
-    theme = "paradise";
+    theme = "onedark";
     extraConfigLua = ''
       _G.theme = "${config.theme}"
     '';
