aa0fa34f07803cd26c767a0ea43d070b9e9568d0
Patrick <fwy996602672@gmail.com>
2024-10-06 07:05:22 +0800

    format

--- config/plug/lsp/lspsaga.nix
+++ config/plug/lsp/lspsaga.nix
@@ -88,17 +88,6 @@
         silent = true;
       };
     }
-
-    # {
-    #   mode = "n";
-    #   key = "gD";
-    #   action = "<cmd>Lspsaga show_line_diagnostics<CR>";
-    #   options = {
-    #     desc = "Goto Declaration";
-    #     silent = true;
-    #   };
-    # }
-
     {
       mode = "n";
       key = "gI";
