003bd84a6f6832563077d07930c79ba05e3ff8eb
Patrick <fwy996602672@gmail.com>
2024-10-20 22:09:19 +0800

    update deprecated settings

--- config/plug/statusline/lualine.nix
+++ config/plug/statusline/lualine.nix
@@ -5,104 +5,108 @@ in
 {
   plugins.lualine = {
     enable = true;
-    globalstatus = true;
-    extensions = [
-      "fzf"
-      "neo-tree"
-    ];
-    disabled_filetypes = {
-      statusline = [
-        "dashboard"
-        "alpha"
-        "starter"
+    settings = {
+      globalstatus = true;
+      extensions = [
+        "fzf"
+        "neo-tree"
       ];
-    };
-    theme = "onedark";
-    inactiveSections = {
-      lualine_x = [
-        "filename"
-        "filetype"
-      ];
-    };
-    sections = {
-      lualine_a = [
-        {
-          name = "mode";
-          icon = " ";
-        }
-      ];
-      lualine_b = [
-        {
-          name = "branch";
-          icon = "";
-        }
-        {
-          name = "diff";
-          extraConfig = {
-            symbols = {
-              added = " ";
-              modified = " ";
-              removed = " ";
+      disabled_filetypes = {
+        statusline = [
+          "dashboard"
+          "alpha"
+          "starter"
+        ];
+      };
+      inactive_sections = {
+        lualine_x = [
+          "filename"
+          "filetype"
+        ];
+      };
+      sections = {
+        lualine_a = [
+          {
+            name = "mode";
+            icon = " ";
+          }
+        ];
+        lualine_b = [
+          {
+            name = "branch";
+            icon = "";
+          }
+          {
+            name = "diff";
+            extraConfig = {
+              symbols = {
+                added = " ";
+                modified = " ";
+                removed = " ";
+              };
             };
-          };
 
-        }
-      ];
-      lualine_c = [
-        {
-          name = "diagnostic";
-          extraConfig = {
-            symbols = {
-              error = " ";
-              warn = " ";
-              info = " ";
-              hint = "󰝶 ";
+          }
+        ];
+        lualine_c = [
+          {
+            name = "diagnostic";
+            extraConfig = {
+              symbols = {
+                error = " ";
+                warn = " ";
+                info = " ";
+                hint = "󰝶 ";
+              };
             };
-          };
-        }
-      ];
-      lualine_x = [
-        {
-          name.__raw = ''
-            function()
-              local icon = " "
-              local status = require("copilot.api").status.data
-              return icon .. (status.message or " ")
-            end,
+          }
+        ];
+        lualine_x = [
+          {
+            name.__raw = ''
+              function()
+                local icon = " "
+                local status = require("copilot.api").status.data
+                return icon .. (status.message or " ")
+              end,
 
-            cond = function()
-             local ok, clients = pcall(vim.lsp.get_clients, { name = "copilot", bufnr = 0 })
-             return ok and #clients > 0
-            end,
-          '';
-        }
-      ];
-      lualine_y = [
-        {
-          name = "filetype";
-          extraConfig = {
-            icon_only = true;
-          };
-        }
-        {
-          name = "filename";
-          extraConfig = {
-            symbols = {
-              modified = "";
-              readonly = "👁️";
-              unnamed = "";
+              cond = function()
+              local ok, clients = pcall(vim.lsp.get_clients, { name = "copilot", bufnr = 0 })
+              return ok and #clients > 0
+              end,
+            '';
+          }
+        ];
+        lualine_y = [
+          {
+            name = "filetype";
+            extraConfig = {
+              icon_only = true;
             };
-          };
-        }
-      ];
-      lualine_z = [
-        {
-          name = "progress";
-        }
-        {
-          name = "location";
-        }
-      ];
+          }
+          {
+            name = "filename";
+            extraConfig = {
+              symbols = {
+                modified = "";
+                readonly = "👁️";
+                unnamed = "";
+              };
+            };
+          }
+        ];
+        lualine_z = [
+          {
+            name = "progress";
+          }
+          {
+            name = "location";
+          }
+        ];
+        options = {
+          theme = "onedark";
+        };
+      };
     };
   };
 }
